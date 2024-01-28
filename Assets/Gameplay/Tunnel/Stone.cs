e28c9b7ddf50adacaf87b6a840152b614cb8c410
Carlos Daniel Robaina Rivero <cdrobayna.personal@gmail.com>
2024-01-28 14:30:36 -0500

    v

--- Assets/Gameplay/Tunnel/Stone.cs
+++ Assets/Gameplay/Tunnel/Stone.cs
@@ -4,14 +4,24 @@ using UnityEngine;
 
 public class Stone : MonoBehaviour
 {
+    [SerializeField] private VoidEventChannel m_StoneStrike;
     [SerializeField] Transform spawnPoint;
     [SerializeField] GameObject stoneFracture;
+
     void OnCollisionEnter(Collision col)
     {
         if (col.gameObject.CompareTag("ground"))
         {
             Instantiate(stoneFracture, transform.position, Quaternion.identity);
             transform.position = spawnPoint.position;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            m_StoneStrike.RaiseEvent();
         }
     }
 
